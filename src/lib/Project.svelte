acde20b1cd7b0cd51982723fda6a0f420ca4f75a
Ashi Kamra <ak118@wellesley.edu>
2025-05-28 12:55:59 -0400

    adding project component

---
+++ src/lib/Project.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let data; //placeholder for data, so that any source can be used
+</script>
+
+<article>
+    <div>
+        <h2> {data.title} </h2>
+        <p> {data.description} </p>
+        {#each data.stack as s}
+            <p>{s}</p>
+        {/each}    
+    </div>
+    <a href={data.link}><img src={data.image} alt="blank"></a>
+
+</article>
+
+<style>
+    article {
+        display: grid;
+    }
+    
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,13 @@
-<h1>projects</h1>
+<script>
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+
+</script>
+
+<svelte:head>
+    <h1>my projects</h1>
+</svelte:head>
+
+{#each projects as p}
+    <Project data={p} />
+{/each}
